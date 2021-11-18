72a5a2ff001e513cf41da265bcb10c16483ceed0
John McMahon <idahosound@gmail.com>
2021-11-18 11:19:40 -0700

    add components folder, reorganize

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./header";
+import Header from "../header";
 import Footer from "./footer";
-import Note from "./note";
+import Note from "../note";
 
 function App(){
     return(
