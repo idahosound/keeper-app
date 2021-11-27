b4a32403980c52d342e95192636a70a61fd7a45d
John McMahon <idahosound@gmail.com>
2021-11-27 01:00:56 -0700

    delete prefab notes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
-import notes from "../notes";
+
 import { isNotEmittedStatement } from "typescript";
 
 function App() {
