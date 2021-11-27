2d9fd9fec03c6722c95e0e1936e0d08ed3959163
John McMahon <idahosound@gmail.com>
2021-11-27 01:38:17 -0700

    set create-area title and note to be stateful

--- src/components/create-area.jsx
+++ src/components/create-area.jsx
@@ -5,11 +5,19 @@ function CreateArea() {
     const [noteTitle, setNoteTitle] = useState("");
     const [note, setNote] = useState("");
 
+    function handleChange(event){
+        if (event.target.name === "title") {
+            setNoteTitle(event.target.value);
+        } else if (event.target.name === "content") {
+            setNote(event.target.value);
+        }       
+    }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input onChange={handleChange} name="title" placeholder="Title" value={noteTitle}/>
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={note}/>
         <button>Add</button>
       </form>
     </div>
